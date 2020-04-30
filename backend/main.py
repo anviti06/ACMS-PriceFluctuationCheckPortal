1ed10dd06657511f277dbe1a5158980885afe8dd
anviti06 <srivastavanviti@gmail.com>
2020-04-30 23:27:44 +0530

    Connection with backend done

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,47 @@
-from flask import (Flask, render_template)
+from flask import (Flask, render_template, request,Blueprint)
 from flask_cors import CORS,cross_origin
-from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, current_user, UserMixin
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
 app = Flask("__main__")
 
+#Defining Global variables
+scheduler = BackgroundScheduler()
+db = SQLAlchemy(app)
+
+#Defining App configurations
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database'
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+#Registering Blueprints - For Home page
+from home import home_bp as home_blueprint
+app.register_blueprint(home_blueprint)
+ 
+#Importing Databases
+from models import *
+
+#Registering Blueprints - For Login page
+from login import login_bp as login_blueprint
+app.register_blueprint(login_blueprint)
+
+#Login Manager
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
+app.run(debug=True,host='0.0.0.0',port=5000)
+
+    
+
+"""
 @app.route("/")
 def my_index():
     return render_template("index.html",token="Hello   world")
@@ -18,7 +57,5 @@ def get_from_react():
     print(data)
     response="Whatever you wish too return"
     return response
+"""
 
-app.run(debug=True,host='127.0.0.1',port=5000)
-flask_cors.CORS(app, resources={ r'/*': {'origins': "*"}}, supports_credentials=True)
-app.config['CORS_HEADERS'] = 'Content-Type'
