eeeca2d0ec9770145f7df6f973269b411a9b38d2
Anviti <39837967+anviti06@users.noreply.github.com>
2020-05-12 12:45:28 +0530

    Update get_product.py

--- backend/app/get_product.py
+++ backend/app/get_product.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, jsonify, json
+from flask_login import current_user, login_required
+from werkzeug.security import check_password_hash
 from .models import db, Product
+from sqlalchemy import update
 prod_bp = Blueprint('prod_bp', __name__)
 
 @prod_bp.route('/product' , methods = ['GET'])
