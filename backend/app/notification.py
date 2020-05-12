5a8210fb457c2a5bbef9dd564239b0af645fda78
Anviti <39837967+anviti06@users.noreply.github.com>
2020-05-12 13:03:21 +0530

    Scheduler Completed

---
+++ backend/app/notification.py
@@ -0,0 +1,12 @@
+"""
+Functions for Notification system
+
+"""
+def raise_notification(id,pid):
+    """
+    If Notification send - return True
+    else return False
+    """
+    print("Customer id :" , id)
+    print("Product id is :" , pid)
+    return True
