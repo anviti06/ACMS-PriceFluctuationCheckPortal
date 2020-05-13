1d829b3ec6d87b3f62300990517f13912f45c674
anviti06 <srivastavanviti@gmail.com>
2020-05-13 19:34:55 +0530

    updated

---
+++ backend/static/react/precache-manifest.8747549a3f52aeaed6de0975f9c09dab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48654b95da19a4bed061d869aff15742",
+    "url": "/react/../../templates/index.html"
+  },
+  {
+    "revision": "d82a07f32549e423493c",
+    "url": "/react/css/main.1f1b6d3a.chunk.css"
+  },
+  {
+    "revision": "6e8df04d9b86de115759",
+    "url": "/react/js/2.6a65eca6.chunk.js"
+  },
+  {
+    "revision": "4247f5b824f6f475f36ec1d67f80a2a8",
+    "url": "/react/js/2.6a65eca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d82a07f32549e423493c",
+    "url": "/react/js/main.ee4c28a6.chunk.js"
+  },
+  {
+    "revision": "af3d07a6c179d6060bf8",
+    "url": "/react/js/runtime-main.baf8f1fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "010b631c85ff9c8e03e2d68eb3364a3e",
+    "url": "/react/media/shop.010b631c.jpg"
+  }
+]);
