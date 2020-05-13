34c750fb5a3d5d873cc23df784638ffdb862fcba
HARSHITA <harshitamaurya11744@gmail.com>
2020-05-13 18:25:49 +0530

    products display working

--- backend/app/static/react/precache-manifest.a9486facf61b05d72801ed9208b51e36.js
+++ backend/app/static/react/precache-manifest.a9486facf61b05d72801ed9208b51e36.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a05382c77dcc6de4860e8948c1523eb5",
+    "revision": "090ee66479cbf239e2289a93ebd434f9",
     "url": "/static/react/../../templates/index.html"
   },
   {
-    "revision": "f8500d2e8424828b1d37",
-    "url": "/static/react/css/main.9146927c.chunk.css"
+    "revision": "7615cf071c7862df358d",
+    "url": "/static/react/css/main.a9285082.chunk.css"
   },
   {
-    "revision": "6080a03b17c050128f19",
-    "url": "/static/react/js/2.3c30d64e.chunk.js"
+    "revision": "ecf998cccc227beaa484",
+    "url": "/static/react/js/2.8446b817.chunk.js"
   },
   {
-    "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/react/js/2.3c30d64e.chunk.js.LICENSE.txt"
+    "revision": "4247f5b824f6f475f36ec1d67f80a2a8",
+    "url": "/static/react/js/2.8446b817.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f8500d2e8424828b1d37",
-    "url": "/static/react/js/main.63cc4ee6.chunk.js"
+    "revision": "7615cf071c7862df358d",
+    "url": "/static/react/js/main.c4205664.chunk.js"
   },
   {
     "revision": "a15feaddd1ae32bb8c0e",
--- backend/main.py
+++ backend/main.py
@@ -4,5 +4,5 @@ from flask import Flask
 from app.app import create_app
 
 app = create_app()
-app.run(debug=True,host='0.0.0.0',port=5000)
+app.run(debug=True,host='127.0.0.1',port=5000)
     
